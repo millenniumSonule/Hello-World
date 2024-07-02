5b470668182f89c5440d2058433a21fefc901bda
millenniumSonule <anshumansonuley@gmail.com>
2024-07-02 19:09:52 +0530

    .

--- server.js
+++ server.js
@@ -1,12 +1,2 @@
 const fs = require('fs');
 
-// Writting files
-
-// synchronusly
-const content = "Some random content"
-
-fs.writeFile('./file', content, err => {
-    if(err){
-        console.log(err);
-    }
-})
