c7563268982bc349285aba1d8738b05780920d80
millenniumSonule <anshumansonuley@gmail.com>
2024-07-11 00:21:38 +0530

    .

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ const app = express();
 const PORT = 8000;
 
 //Routes
-app.get("/users/api", (req,res) => { 
-   res.json(users);
+app.get("/users/api", (req, res) => {
+  res.json(users);
 });
 
-app.get('/users', (req,res) => {
-  
-  const html = `
-    <ul> ${users.map((users) => `<li>${users.first_name}</li>`).join} </ul>
-  `;
+app.get('/users', (req, res) => {
+  const html = `<ul> ${users.map((users) => `<li>${users.first_name}</li>`).join} </ul>`;
   res.send(html);
-  
 })
 
 
 
 
 
-app.listen(PORT, ()=> console.log(`Server running at port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
